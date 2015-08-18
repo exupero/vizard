032e17ffc6eada0cdbdff693cbd400f6652a2473
Eric Shull <shull@atomicobject.com>
2015-08-17 20:51:02 -0400

    Add ability to save png.

--- vizard.go
+++ vizard.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"encoding/base64"
+	"errors"
 	"flag"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+
+	"github.com/toqueteos/webbrowser"
 )
 
 func static(name, contentType string) func(http.ResponseWriter, *http.Request) {
@@ -20,6 +26,19 @@ func static(name, contentType string) func(http.ResponseWriter, *http.Request) {
 	}
 }
 
+func staticTemplate(name, contentType string, data interface{}) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		asset, err := Asset(name)
+		if err != nil {
+			panic(err)
+		}
+		w.Header().Set("Content-Type", contentType)
+
+		t := template.Must(template.New(name).Parse(string(asset)))
+		t.Execute(w, data)
+	}
+}
+
 func data(content []byte, contentType string) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", contentType)
@@ -27,14 +46,21 @@ func data(content []byte, contentType string) func(http.ResponseWriter, *http.Re
 	}
 }
 
+func handleError(w http.ResponseWriter) {
+	if err := recover(); err != nil {
+		http.Error(w, fmt.Sprintf("%s", err), http.StatusInternalServerError)
+	}
+}
+
 func main() {
 	port := flag.Int("port", 8000, "HTTP service port")
-	contentType := flag.String("content-type", "application/json", "Content-Type")
+	svgFilename := flag.String("svg", "", "Filename to save SVG to")
 	flag.Parse()
 
 	if flag.NArg() < 1 {
 		log.Fatal("Script name required as first argument")
 	}
+	scriptFilename := flag.Arg(0)
 
 	stat, err := os.Stdin.Stat()
 	if err != nil {
@@ -51,16 +77,45 @@ func main() {
 		content = []byte{}
 	}
 
-	js, err := ioutil.ReadFile(flag.Arg(0))
-	if err != nil {
-		log.Fatal("Script:")
-	}
+	http.HandleFunc("/svg", func(w http.ResponseWriter, req *http.Request) {
+		defer handleError(w)
 
-	http.HandleFunc("/data", data(content, *contentType))
+		if len(*svgFilename) == 0 {
+			panic(errors.New("No filename given for saving an SVG"))
+		}
+
+		bin, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		contents := strings.TrimPrefix(string(bin), "data:image/svg+xml;base64,")
+		svg, err := base64.StdEncoding.DecodeString(contents)
+		if err != nil {
+			panic(err)
+		}
+
+		f, err := os.Create(*svgFilename)
+		if err != nil {
+			panic(err)
+		}
+
+		_, err = f.Write(svg)
+		if err != nil {
+			panic(err)
+		}
+	})
+
 	http.HandleFunc("/bundle.js", static("bundle.js", "application/javascript"))
 	http.HandleFunc("/styles.css", static("styles.css", "text/css"))
-	http.HandleFunc("/script.js", data(js, "application/javascript"))
-	http.HandleFunc("/", static("index.html", "text/html"))
+	http.HandleFunc("/script.js", func(w http.ResponseWriter, req *http.Request) {
+		http.ServeFile(w, req, scriptFilename)
+	})
+	http.HandleFunc("/", staticTemplate("index.html", "text/html", template.JS(content)))
+
+	go func(){
+		webbrowser.Open(fmt.Sprintf("0.0.0.0:%d", *port))
+	}()
 
 	log.Println("Starting server...")
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", *port), nil))
--- vizard.js
+++ vizard.js
@@ -1,3 +1,12 @@
 import d3 from 'd3';
+import {svgAsDataUri} from 'save-svg-as-png';
 
 window.d3 = d3;
+
+d3.select('#save').on('click', function() {
+  svgAsDataUri(d3.select('main svg').node(), {}, function(uri) {
+    var r = new XMLHttpRequest();
+    r.open('POST', '/svg', true);
+    r.send(uri);
+  });
+});
